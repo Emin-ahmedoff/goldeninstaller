7a4658e4b7bbc16aa2fed221dceede25c6f2541a
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2021-11-23 16:14:55 +0400

    Update __main__.py

--- golden_installer/__main__.py
+++ golden_installer/__main__.py
@@ -66,7 +66,7 @@ async def botlog (String, Api, Hash):
     ))
     KanalId = KanalId.chats[0].id
 
-    Photo = await Client.upload_file(file='golden.png')
+    Photo = await Client.upload_file(file='golden.jpg')
     await Client(EditPhotoRequest(channel=KanalId, 
         photo=Photo))
     msg = await Client.send_message(KanalId, LANG['DONT_LEAVE'])
